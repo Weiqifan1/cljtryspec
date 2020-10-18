06d389f31e302f57168837e914814ed7612b0297
Weiqifan1 <cmlykke@hotmail.com>
2020-10-18 18:20:59 +0200

    first commit

---
+++ src/clj_anki_generator/core.clj
@@ -0,0 +1,11 @@
+(ns clj-anki-generator.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/clj_anki_generator/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-anki-generator.core-test
+  (:require [clojure.test :refer :all]
+            [clj-anki-generator.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
